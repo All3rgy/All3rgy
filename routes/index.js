65dedd1da50cda142b279d9d24426c5d98008e1b
Simon Chen <sc4900@nyu.edu>
2017-03-14 14:31:36 -0400

    HandlesBarJS&MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+
+const mongodb_url = "mongodb://admin:root@ds131340.mlab.com:31340/all3rgy";
+var db;
+
+MongoClient.connect(mongodb_url, (err, database) => {
+    if (err) return console.log(err);
+    db = database;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+//Search Handler
+router.post('/search', (req,res) => {
+    console.log(req.body['search']);
+    res.redirect('/');
+});
+
+router.get('/order', function(req, res, next) {
+
+    var cursor = db.collection('order').find();
+    
+    db.collection('order').find().toArray(function(err, results) {
+	console.log(results);
+	// results = results[0]['name'];
+	return res.render('layout', {foods: results});
+	// res.send(results);
+	
+	// send HTML file populated with quotes here
+    });
+
 });
 
 module.exports = router;
