76b1de4a85c38fc73a90523513451960ed533249
Simon Chen <sc4900@nyu.edu>
2017-04-25 21:18:18 -0400

    Data

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var db;
 
 
 // Credentials for calling Edamam API
-var edamambase = "https://api.edamam.com/search?q=";
+var edamambase = "http://api.edamam.com/search?q=";
 var appID = "27511a3c";
 var appKey = "942220d8bf3bb23397fa7eb732745641";
 
@@ -96,7 +96,7 @@ router.post('/search', (req,res) => {
     if (tagquery != undefined){
 	var health = "";
 	for (var i = 0; i < tagquery.length; i++)
-	    health += tagquery[i]+",";
+	    health += tagquery[i];
 	
 	edamamurl = edamambase + req.body['search'] + "&app_id=" + appID + "&app_key=" + appKey + "&health="+health;
 
@@ -106,89 +106,97 @@ router.post('/search', (req,res) => {
 
     console.log(edamamurl);
 
-    var http = require("http");
 
-    var request = http.get(url, function (response) {
+    var request = require("request");
 
+    var finalquery;
+    request(edamamurl, function(error, response, body) {
+	data = JSON.parse(body);
+	// console.log(data["hits"]);
+	finalquery = data["hits"];
+
+	return res.render('results', {foods: finalquery, prevsearch: {"name" : req.body['search']}});
     });
-
-    if (req.body['type'] != undefined && typeof req.body['type'] == "string")
-    {
-	console.log(req.body['type'].split());
-	typequery = req.body['type'].split();
-	// typequery = tmp;
-    }
-
-    // console.log(typeof typequery);
     
-    // Checks if users selected tags, only do specific query
-    if (allergyquery == undefined && typequery == undefined)
-    		cursor = db.collection('order').find({name:namequery});
-	    else if (typequery != undefined){
+
+    
+    // if (req.body['type'] != undefined && typeof req.body['type'] == "string")
+    // {
+    // 	console.log(req.body['type'].split());
+    // 	typequery = req.body['type'].split();
+    // 	// typequery = tmp;
+    // }
+
+    // // console.log(typeof typequery);
+    
+    // // Checks if users selected tags, only do specific query
+    // if (allergyquery == undefined && typequery == undefined)
+    // 		cursor = db.collection('order').find({name:namequery});
+    // 	    else if (typequery != undefined){
     		
-		var filterquery;
-		if (tagquery != undefined)
-    		    filterquery = tagquery.concat(typequery);
-		else
-		    filterquery = typequery;
+    // 		var filterquery;
+    // 		if (tagquery != undefined)
+    // 		    filterquery = tagquery.concat(typequery);
+    // 		else
+    // 		    filterquery = typequery;
 		
-		console.log(filterquery);
-		console.log(namequery);
+    // 		console.log(filterquery);
+    // 		console.log(namequery);
 		
-    		cursor = db.collection('order').find({name:namequery, tags: {$in : filterquery}});
-	    }
-	    else
-    		// Checks if there are multiple allergies
-    		cursor = db.collection('order').find({name:namequery, tags: {$in : tagquery }});
+    // 		cursor = db.collection('order').find({name:namequery, tags: {$in : filterquery}});
+    // 	    }
+    // 	    else
+    // 		// Checks if there are multiple allergies
+    // 		cursor = db.collection('order').find({name:namequery, tags: {$in : tagquery }});
 
-	    // console.log(cursor);
-	    cursor.toArray(function(err, results) {
-    		console.log(results);
+    // 	    // console.log(cursor);
+    // 	    cursor.toArray(function(err, results) {
+    // 		console.log(results);
 
-    		//Final query to be pushed to db
-    		finalquery = [];
+    // 		//Final query to be pushed to db
+    // 		finalquery = [];
 
-    		//Checks to see if there are any results
-    		if (results != undefined){
+    // 		//Checks to see if there are any results
+    // 		if (results != undefined){
 
-    		    //Double checks allergies to see if the allergy is in the result
-    		    for (const r of results){
-    			clearq = true;
+    // 		    //Double checks allergies to see if the allergy is in the result
+    // 		    for (const r of results){
+    // 			clearq = true;
 			
-			console.log("looping");
-			console.log(tagquery);
+    // 			console.log("looping");
+    // 			console.log(tagquery);
 
-			if (tagquery != undefined){
-    			    for (const t of tagquery){
-    				console.log(t in tagquery);
-    				if (!(tagquery.indexOf(t) != -1)){
-    				    clearq = false;
-    				    break;
-    				}
-    			    }
+    // 			if (tagquery != undefined){
+    // 			    for (const t of tagquery){
+    // 				console.log(t in tagquery);
+    // 				if (!(tagquery.indexOf(t) != -1)){
+    // 				    clearq = false;
+    // 				    break;
+    // 				}
+    // 			    }
 			    
-			}
+    // 			}
 
-			if (clearq)
-    			    finalquery.push(r);
+    // 			if (clearq)
+    // 			    finalquery.push(r);
 			
-    		    }
-    		}
+    // 		    }
+    // 		}
 
-		// finalquery = [{"name": "chicken rice", "owner": "Ashley"},
-		// 	      {"name": "chicken over rice"},
-		// 	      {"name": "chicken fried rice"}];
+    // 		// finalquery = [{"name": "chicken rice", "owner": "Ashley"},
+    // 		// 	      {"name": "chicken over rice"},
+    // 		// 	      {"name": "chicken fried rice"}];
 		
-		console.log(finalquery);
-		console.log(namequery);
+    // 		console.log(finalquery);
+    // 		console.log(namequery);
 		
-		// send HTML file populated with quotes here
-		return res.render('results', {foods: finalquery, prevsearch: {"name" : req.body['search']}});
+    // 		// send HTML file populated with quotes here
+    // 		return res.render('results', {foods: finalquery, prevsearch: {"name" : req.body['search']}});
 
 		
-	    });
+    // });
 	    
-	   });
+});
 
 
 router.get('/order', function(req, res, next) {
